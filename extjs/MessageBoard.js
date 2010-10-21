7557b3f840e676e5c631ab667090003c5694cae6
psaechiew <greazalv@gmail.com>
2010-10-21 12:02:16 +0700

    adjust sticky notes style

--- extjs/MessageBoard.js
+++ extjs/MessageBoard.js
@@ -3,10 +3,8 @@
  */
 
 var MessageBoard = (function () {
-    /*!
-     * Message Board functions
-     */
-    var dateTimeToText = function(dt) {
+
+    var dateTimeToText = function(dt){
 		    var month = dt.getMonth() + 1
 		    var day = dt.getDate()
 		    var year = dt.getFullYear()
@@ -15,80 +13,66 @@ var MessageBoard = (function () {
 		    return ("" + hour + ":" + min + " " + month + "/" + day + "/" + year)
     }
 
-    var currentTime = function() {
+    var currentTime = function(){
 		var currentTime = new Date()
 		return dateTimeToText(currentTime)
     }
 
-    var message = function(text) {
-        var result = '';
-        result += '<div class="message">';
-        result += '<div class="message-top">';
-        result += 'User';
-        result += '</div>';
-        result += '<div class="message-body">';
-        result += text;
-        result += '</div>';
-        result += '<div class="message-bottom">';
-        result += currentTime();
-        result += '</div>';
-        result += '</div>';
-        return result;
+    var messagePacking = function(message){
+	    var m = ""
+   	    m = m + "<div class='by'>Apirak</div>"
+	    m = m + "<div class='message'>" + message + "</div>"
+	    m = m + "<div class='date'>" + currentTime() + "</div>"
+	    m = "<div class='stickynote'>" + m + "</div>"
+	    return m
     }
 
-    /*!
-     * Message Board
-     */
-    var mesBoard = [
-        {
-            region: 'center',
-            layout: 'fit',
-            items: [{
-                autoScroll: true,
-                html: '<div id="mboard">' + message('This quotation need to ' +
-                    'prove by p\'Suda befor send to customer.') + '</div>'
-            }]
-        },{
-            region: 'south',
-            height: 150,
-            split: false,
-            title: 'Post a message',
-            items: [{
-                xtype: 'htmleditor',
-                id: 'posttext',
-	            enableColors: false,
-                enableAlignments: false,
-	            enableFont: false,
-	            enableLinks: false,
-	            enableFontSize: false,
-    	        enableSourceEdit: false
-            }],
-            bbar: {
-                items:[{
-                    text: 'Submit',
-                    iconCls: 'save',
-    	            handler: function(){
-                        var dh = Ext.DomHelper;
-						postText = Ext.getCmp('posttext').getValue();
-		                mboard = Ext.getDom('mboard').innerHTML;
-		                mboard += message(postText);
-		                Ext.getDom('mboard').innerHTML = mboard;
-		                Ext.getCmp('posttext').setValue("");
-		            }
-                }]
+    result = {
+                region: 'east',
+                title: 'Message Board',
+                layout: 'vbox',
+                width: 300,
+                split: true,
+                collapsible: true,
+                layoutConfig: {
+                    align: 'stretch',
+                    pack: 'start'
+                },
+                items: [{
+                    html: "<div id='log-list'><div class='stickynote'>" +
+                        "<div class='by'>Apirak</div>" +
+                        "<div class='message'>This quotation need to prove " +
+                        "by p'Suda befor send to customer.</div>" +
+                        "<div class='date'>14:13 10/14/2010</div></div></div>",
+                    border:false,
+                    flex:1
+                },{
+                    xtype:'htmleditor',
+                    id:'messageLog',
+                    border: false,
+                    height:100,
+                    anchor:'98%',
+			        enableColors: false,
+                    enableAlignments: false,
+			        enableFont: false,
+			        enableLinks: false,
+			        enableFontSize: false,
+			        enableSourceEdit: false
+                }],
+                bbar: {
+                    items:[{
+                        text: 'Submit',
+                        iconCls: 'save',
+			            handler: function(){
+                            var dh = Ext.DomHelper;
+							temp_message = Ext.getCmp('messageLog').getValue();
+			                logListHTML = Ext.getDom('log-list').innerHTML;
+			                loglistHTML = logListHTML + messagePacking(temp_message);
+			                Ext.getDom('log-list').innerHTML = loglistHTML;
+			                Ext.getCmp('messageLog').setValue("");
+			            }
+                    }]
+                }
             }
-        }
-    ];
-
-    var result = {
-        region: 'east',
-        title: 'Message Board',
-        layout: 'border',
-        items: mesBoard,
-        width: 250,
-        split: true,
-        collapsible: true
-    };
-
     return result; // Export the module
 }());
